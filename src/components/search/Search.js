40c937c97ea670cec1b2a8b09a8da5d9e6073803
macosta2015 <macosta2015@my.fit.edu>
2022-08-21 14:18:56 -0400

    Editing the photo

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -10,7 +10,7 @@ function Search() {
                 <div className="left">
                     <h2>LUXURY INCLUDED VACATIONS FOR TWO PEOPLE</h2>
                     <p>Come experience the very pinnacle of luxury Caribbean all-inclusive vacations for couples at Sandals Resorts. Our luxury beach resorts, set along the most gorgeous tropical settings and exquisite beaches in Saint Lucia, Jamaica, Antigua, The Bahamas, Grenada, Barbados and Curaçao, feature unlimited gourmet dining, unique bars serving premium liquors and wines, and every land and water sport, including complimentary green fees at our golf resorts and PADI® certified scuba diving at most resorts. If you are planning a wedding, Sandals is the leader in Caribbean destination weddings and honeymoon packages.</p>
-                    <div className="search-col-2">
+                    {/* <div className="search-col-2">
                         <div className="box">
                             <div>
                                 <img src={Gold} alt="/" style={{ marginRight: '1rem' }} />
@@ -27,7 +27,7 @@ function Search() {
                                 <button>View Packages</button>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="right">
                     <div className="promo">
@@ -36,7 +36,7 @@ function Search() {
                         <p className="offers">VIEW ALL CURRENT OFFERS</p>
                     </div>
                     <form>
-                        <div className="input-wrap">
+                        {/* <div className="input-wrap">
                             <label>Destination</label>
                             <select>
                                 <option value="1">Grande Antigua</option>
@@ -47,8 +47,8 @@ function Search() {
                                 <option value="1">Maldives</option>
                                 <option value="1">Barbados</option>
                             </select>
-                        </div>
-                        <div className="date">
+                        </div> */}
+                        {/* <div className="date">
                             <div className="input-wrap">
                                 <label>Check-In</label>
                                 <input type="date" />
@@ -57,7 +57,7 @@ function Search() {
                                 <label>Check-Out</label>
                                 <input type="date" />
                             </div>
-                        </div>
+                        </div> */}
                         <button>Rates & Availabilities </button>
                     </form>
                 </div>
